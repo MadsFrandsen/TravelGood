90398eede0c5294dd476d64ca6e1fe2a27916fe8
Jonas Karlsson <minipost@gmail.com>
2014-10-28 13:38:42 +0100

    Test TravelGoodSOAP, added test methods

---
+++ TestTravelGoodSOAP/test/dtu/ws/travelgood/SOAPTest/C1.java
@@ -0,0 +1,29 @@
+package dtu.ws.travelgood.SOAPTest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * C1 (cancel booking) Create an itinerary with three bookings (mixed flights
+ * and hotels) and book it. Get the itinerary and make sure that the booking
+ * status is confirmed for each entry. Cancel the trip and check that now the
+ * booking status is cancelled for all bookings of the itinerary.
+ *
+ * @author Jonas Karlsson (S143341)
+ * @return
+ */
+public class C1 {
+
+    /**
+     *
+     */
+    public C1() {
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void testC1() {
+    }
+}
---
+++ TestTravelGoodSOAP/test/dtu/ws/travelgood/SOAPTest/P1.java
@@ -0,0 +1,33 @@
+package dtu.ws.travelgood.SOAPTest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * P1 (planning and booking) Plan a trip by first planning a flight (i.e.
+ * getting a list of flights and then adding a flight to the itinerary), then by
+ * planning a hotel, another flight, a third flight, and finally a hotel. Ask
+ * for the itinerary and check that it is correct using JUnit’s assert
+ * statements – i.e. assertEquals, assertTrue, . . . – in particular, that the
+ * booking status for each item is unconfirmed. Book the itinerary and ask again
+ * for the itinerary. Check that each booking status is now confirmed
+ *
+ * @author Jonas Karlsson (S143341)
+ * @return
+ */
+public class P1 {
+
+    /**
+     *
+     */
+    public P1() {
+    }
+
+    
+    /**
+     *
+     */
+    @Test
+    public void testP1() {
+    }
+}
