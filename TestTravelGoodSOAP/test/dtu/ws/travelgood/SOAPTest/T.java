ef081a8136203aeebb1016e6d44e24b3edfab55b
Jonas Karlsson <minipost@gmail.com>
2014-11-03 11:19:41 +0100

    T

---
+++ TestTravelGoodSOAP/test/dtu/ws/travelgood/SOAPTest/T.java
@@ -0,0 +1,109 @@
+package dtu.ws.travelgood.SOAPTest;
+
+import java.util.ArrayList;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+import ws.travelgood.xml.AccommodationOptions;
+import ws.travelgood.xml.Itinerary;
+import ws.travelgood.xml.Stay;
+import ws.travelgood.xml.Travel;
+import ws.travelgood.xml.TravelOptions;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author jonas
+ */
+public class T {
+
+    public DatatypeFactory df;
+    public String[] personNames, destinations, unbookableDestinations;
+    public XMLGregorianCalendar[] dates;
+    public int[] creditCardNumbers;
+    public Travel[] travels, itinearyTravels;
+    public Stay[] stays, itinearyStays;
+    public String itinearyID;
+    public Itinerary itineary; 
+
+    public T() throws DatatypeConfigurationException {
+        df = DatatypeFactory.newInstance();
+        personNames = new String[3];
+        personNames[0] = "Poul Thomsen";
+        personNames[1] = "Søren Frederiksen";
+        personNames[2] = "Peter Pan";
+
+        destinations = new String[3];
+        destinations[0] = "Copenhagen";
+        destinations[1] = "Berlin";
+        destinations[2] = "Oslo";
+        
+        unbookableDestinations = new String[3];
+        unbookableDestinations[0] = "Kiev";
+        unbookableDestinations[1] = "Hamburg";
+        
+        dates = new XMLGregorianCalendar[4];
+        dates[0] = df.newXMLGregorianCalendar("2015-09-15");
+        dates[1] = df.newXMLGregorianCalendar("2015-09-16");
+        dates[2] = df.newXMLGregorianCalendar("2015-09-17");
+        dates[3] = df.newXMLGregorianCalendar("2015-09-18");
+
+        creditCardNumbers = new int[2];
+        creditCardNumbers[0] = 1121222224;
+        creditCardNumbers[1] = 1121222224;
+
+    }
+
+  
+
+    public  boolean addFlight(java.lang.String itineraryID, int bookingNumber) {
+        ws.travelgood.TravelgoodService service = new ws.travelgood.TravelgoodService();
+        ws.travelgood.ItineraryPortType port = service.getTravelgoodPort();
+        return port.addFlight(itineraryID, bookingNumber);
+    }
+
+    public  boolean addHotel(java.lang.String itineraryID, int bookingNumber) {
+        ws.travelgood.TravelgoodService service = new ws.travelgood.TravelgoodService();
+        ws.travelgood.ItineraryPortType port = service.getTravelgoodPort();
+        return port.addHotel(itineraryID, bookingNumber);
+    }
+
+    public  boolean bookItinerary(java.lang.String itineraryID, int creditCardNumber, java.lang.String cardOwner) {
+        ws.travelgood.TravelgoodService service = new ws.travelgood.TravelgoodService();
+        ws.travelgood.ItineraryPortType port = service.getTravelgoodPort();
+        return port.bookItinerary(itineraryID, creditCardNumber, cardOwner);
+    }
+
+    public  boolean cancelItinerary(java.lang.String itineraryID) {
+        ws.travelgood.TravelgoodService service = new ws.travelgood.TravelgoodService();
+        ws.travelgood.ItineraryPortType port = service.getTravelgoodPort();
+        return port.cancelItinerary(itineraryID);
+    }
+
+    public  String createItinerary(java.lang.String personName) {
+        ws.travelgood.TravelgoodService service = new ws.travelgood.TravelgoodService();
+        ws.travelgood.ItineraryPortType port = service.getTravelgoodPort();
+        return port.createItinerary(personName);
+    }
+
+    public  TravelOptions getFlights(java.lang.String arrivalDestination, javax.xml.datatype.XMLGregorianCalendar time, java.lang.String departureDestination) {
+        ws.travelgood.TravelgoodService service = new ws.travelgood.TravelgoodService();
+        ws.travelgood.ItineraryPortType port = service.getTravelgoodPort();
+        return port.getFlights(arrivalDestination, time, departureDestination);
+    }
+
+    public  AccommodationOptions getHotels(javax.xml.datatype.XMLGregorianCalendar start, javax.xml.datatype.XMLGregorianCalendar end, java.lang.String city) {
+        ws.travelgood.TravelgoodService service = new ws.travelgood.TravelgoodService();
+        ws.travelgood.ItineraryPortType port = service.getTravelgoodPort();
+        return port.getHotels(start, end, city);
+    }
+
+    public  Itinerary getItinerary(java.lang.String id) {
+        ws.travelgood.TravelgoodService service = new ws.travelgood.TravelgoodService();
+        ws.travelgood.ItineraryPortType port = service.getTravelgoodPort();
+        return port.getItinerary(id);
+    }
+}
