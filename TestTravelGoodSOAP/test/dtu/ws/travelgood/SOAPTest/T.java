07a4d47f4511e58f8c55659ff2facba63b4b71a2
Jonas Karlsson <minipost@gmail.com>
2014-11-18 10:35:26 +0100

    Update of test and travel data

--- TestTravelGoodSOAP/test/dtu/ws/travelgood/SOAPTest/T.java
+++ TestTravelGoodSOAP/test/dtu/ws/travelgood/SOAPTest/T.java
@@ -1,5 +1,10 @@
 package dtu.ws.travelgood.SOAPTest;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
@@ -19,46 +24,99 @@ import ws.travelgood.xml.TravelOptions;
  */
 public class T {
 
+    class FlightOption {
+
+        @Override
+        public String toString() {
+            return "FlightOption{" + "airline=" + airline + ", source=" + source + ", destination=" + destination + ", arrival=" + arrival + ", departure=" + departure + '}';
+        }
+        public String airline, source, destination;
+        XMLGregorianCalendar arrival, departure;
+    }
+    FlightOption[] bookable, unbookable;
     public DatatypeFactory df;
-    public String[] personNames, destinations, unbookableDestinations;
-    public XMLGregorianCalendar[] dates;
+    public String[] personNames;
     public int[] creditCardNumbers;
     public Travel[] travels, itinearyTravels, bookableTravels;
-  
     public Stay[] stays, itinearyStays;
     public String itinearyID;
-    public Itinerary itineary; 
+    public Itinerary itineary;
 
     public T() throws DatatypeConfigurationException {
         df = DatatypeFactory.newInstance();
+        loadFlightOptions();
+
         personNames = new String[3];
         personNames[0] = "Poul Thomsen";
         personNames[1] = "Søren Frederiksen";
         personNames[2] = "Peter Pan";
-        
-        bookableTravels=new Travel[6];
-        
 
-        destinations = new String[4];
-        destinations[0] = "";
-        destinations[1] = "";
-        destinations[2] = "";
-        destinations[3] = "";
-        
-        unbookableDestinations = new String[3];
-        unbookableDestinations[0] = "Kiev";
-        unbookableDestinations[1] = "Hamburg";
-        
-        dates = new XMLGregorianCalendar[4];
-        dates[0] = df.newXMLGregorianCalendar("2015-09-15");
-        dates[1] = df.newXMLGregorianCalendar("2015-09-16");
-        dates[2] = df.newXMLGregorianCalendar("2015-09-17");
-        dates[3] = df.newXMLGregorianCalendar("2015-09-18");
+        bookableTravels = new Travel[6];
 
         creditCardNumbers = new int[2];
         creditCardNumbers[0] = 1121222224;
         creditCardNumbers[1] = 1121222224;
 
+    }
+
+    public void loadFlightOptions() {
+        bookable = new FlightOption[1];
+        unbookable = new FlightOption[1];
+
+
+        String flightData = "../LameDuck/src/java/LameDuck/flightsdata.csv";
+        ArrayList<FlightOption> bookableList = new ArrayList<>();
+        ArrayList<FlightOption> unbookableList = new ArrayList<>();
+
+        try {
+            File f = new File(flightData);
+
+            Scanner in = new Scanner(f);
+            String headers = in.nextLine(); // skip first line as it is only headers
+            FlightOption fo = new FlightOption();
+
+            DatatypeFactory df = DatatypeFactory.newInstance();
+            while (in.hasNext()) {
+                String[] flightInfo = in.nextLine().split(";"); // Excel data is seperated by ;
+
+                fo.airline = flightInfo[0];
+                fo.source = flightInfo[1];
+                String departureDate = flightInfo[2];
+                String departureTime = flightInfo[3];
+                fo.destination = flightInfo[4];
+                String arrivalDate = flightInfo[5];
+                String arrivalTime = flightInfo[6];
+
+                /* Convert string date to Gregorian Calendar */
+                String departureDay = departureDate.substring(0, 2);
+                String departureMonth = departureDate.substring(2, 4);
+                String departureYear = departureDate.substring(4, 8);
+                String departureHour = departureTime.substring(0, 2);
+                String departureMinute = departureTime.substring(2, 4);
+                GregorianCalendar departure = new GregorianCalendar(Integer.parseInt(departureYear), Integer.parseInt(departureMonth), Integer.parseInt(departureDay), Integer.parseInt(departureHour), Integer.parseInt(departureMinute));
+                fo.departure = df.newXMLGregorianCalendar(departure);
+
+                String arrivalDay = departureDate.substring(0, 2);
+                String arrivalMonth = departureDate.substring(2, 4);
+                String arrivalYear = departureDate.substring(4, 8);
+                String arrivalHour = arrivalDate.substring(0, 2);
+                String arrivalMinute = arrivalDate.substring(2, 4);
+                GregorianCalendar arrival = new GregorianCalendar(Integer.parseInt(arrivalYear), Integer.parseInt(arrivalMonth), Integer.parseInt(arrivalDay), Integer.parseInt(arrivalHour), Integer.parseInt(arrivalMinute));
+                fo.arrival = df.newXMLGregorianCalendar(arrival);
+                //System.out.println(fo);
+                if ("Disney".equals(fo.airline)) {
+                    unbookableList.add(fo);
+                } else {
+                    bookableList.add(fo);
+
+                }
+
+            }
+            bookable = bookableList.toArray(bookable);
+            unbookable = unbookableList.toArray(bookable);
+        } catch (Exception e) {
+            System.err.println("File not found");
+        }
     }
 
     public boolean addFlight(java.lang.String itineraryID, int bookingNumber) {
@@ -108,40 +166,4 @@ public class T {
         ws.travelgood.ItineraryPortType port = service.getItineraryPortTypeBindingPort();
         return port.getHotels(itineraryID, start, end, city);
     }
-    
-    
-    class Travel {
-    public String airline;
-    public String departure;
-    public String distination;
-    public XMLGregorianCalendar arrivalDate;
-    public String arrivalTime;
-    public String departureDate;
-    
-    }
 }
-
-//Airline;      Source; Departure_Date; Departure_Time; Destination; Arrival_Date; Arrival_Time
-//SAS;          CPH;    24122014;       1430;           BKK;        25122014;       600
-//SAS;          CPH;    24122014;       1630;           BKK;        25122014;       800
-//SAS;          CPH;    24122014;       1830;           BKK;        25122014;       1000
-//SAS;          BKK;    31122014;       130;            CPH;        1012015;        730
-//SAS;          BKK;    31122015;       230;            CPH;        1012016;        830
-//SAS;          BKK;    31122016;       330;            CPH;        1012017;        930
-//Thai;         BKK;    27122014;       1530;           SFO;        28122014;       700
-//Thai;         BKK;    27122015;       1700;           SFO;        28122014;       930
-//Thai;         SFO;    2022015;        1100;           BKK;        3022015;        800
-//Thai;         SFO;    2022016;        1500;           BKK;        3022015;        1200
-//Lufthansa;    FRA;    26122014;       1200;           SFO;        26122014;       2000
-//Lufthansa;    FRA;    26122014;       1400;           SFO;        26122014;       2200
-//Lufthansa;    FRA;    26122014;       1600;           SFO;        26122014;       2330
-//Lufthansa;    SFO;    5012015;        1200;           FRA;        5012015;        1830
-//Lufthansa;    SFO;    5012016;        1430;           FRA;        5012016;        2000
-//Lufthansa;    SFO;    5012017;        1645;           FRA;        5012017;        2245
-//Air France;   CDG;    10012015;       930;            YVR;        10012015;       1430
-//Air France;   CDG;    10012015;       1130;           YVR;        10012015;       1630
-//Air France;   YVR;    15012015;       1030;           CDG;        15012015;       1830
-//Air France;   YVR;    15012015;       1330;           CDG;        15012015;       2130
-//KLM;          AMS;    12122014;       1730;           DAR;        12122014;       2300
-//KLM;          DAR;    15122014;       1500;           AMS;        15122014;       2100
-//Disney;       Andeby; 1012015;        1200;           Moon;       1012015;        2300
